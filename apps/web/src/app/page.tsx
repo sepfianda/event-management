b2b9f45152bfb535dc136ec6a6c9bc84d1fd37b3
sepfianda <sepfiandaekawidhira23@gmail.com>
2024-07-30 22:37:53 +0700

    fix: conflict

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,4 @@
-import CarouselCard from '@/components/card/CarouselCard';
 import EventCard from '@/components/card/EventCard';
-import MarqueeCard from '@/components/card/MarqueeCard';
 import CityList from '@/components/city/CityList';
 import HeroSection from '@/components/hero/HeroSection';
 
