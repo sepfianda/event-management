a73d3008d874077ae96af583f8244ff6bb2c55ba
sepfianda <sepfiandaekawidhira23@gmail.com>
2024-08-05 14:53:29 +0700

    feat: add login fix

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,67 +1,83 @@
-"use client";
+'use client';
 
-import Forminput from '@/components/Forminput';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useFormik } from 'formik';
+import { Key, Mail } from 'lucide-react';
+import Link from 'next/link';
+import { validationSchema } from './validationSchema';
+import useLogin from '@/hooks/api/auth/useLogin';
 
 const Login = () => {
-  const {values, errors, touched, handleChange, handleBlur, handleSubmit} = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    //validationSchema,
-    onSubmit: (values) => {
-    //register(values);
-    },
-  });
+  const {login} = useLogin();
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      validationSchema,
+      onSubmit: (values) => {
+        login(values);
+      },
+    });
 
   return (
-    <main className="container mx-auto h-[90vh] px-4">
-    <div className="mt-0 pt-40 flex justify-center">
-      <Card className="w-[450px]">
-        <CardHeader>
-          <CardTitle className="text-center text-3xl text-primary">
-            Welcome to Eventify
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="grid w-full items-center gap-4">
-              <Forminput
-                name="email"
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="hidden h-full items-center justify-center rounded-r-2xl border border-r-2  bg-white md:flex">
+        <h1 className="text-lg font-bold text-[#393e41]">
+          EVENT<span className="text-lg text-[#e94f37]">IFY</span>
+        </h1>
+      </div>
+      <div className="h-[600px] max-w-full">
+        <form
+          onSubmit={handleSubmit}
+          className="mx-auto flex h-full max-w-[500px] flex-col justify-center space-y-5 px-10"
+        >
+          <h1 className="text-xl font-bold md:py-5">Login</h1>
+
+          <div className="space-y-2">
+            <h1 className="text-[15px] font-semibold">Email</h1>
+            <label className="input input-bordered flex h-[40px] items-center gap-2 rounded-lg">
+              <Mail color="gray" size={20} />
+              <input
                 type="text"
-                label="Email"
+                className="grow"
                 placeholder="Email"
+                name="email"
                 value={values.email}
-                error={errors.email}
-                isError={!!touched.email && !!errors.email}
-                handleChange={handleChange}
-                handleBlur={handleBlur}
+                onChange={handleChange}
               />
+            </label>
+          </div>
 
-              <Forminput
-                name="password"
+          <div className="space-y-2">
+            <h1 className="text-[15px] font-semibold">Password</h1>
+            <label className="input input-bordered flex h-[40px] items-center gap-2 rounded-lg">
+              <Key color="gray" size={20} />
+              <input
                 type="password"
-                label="Password"
+                className="grow"
                 placeholder="Password"
+                name="password"
                 value={values.password}
-                error={errors.password}
-                isError={
-                  !!touched.password && !!errors.password
-                }
-                handleChange={handleChange}
-                handleBlur={handleBlur}
+                onChange={handleChange}
               />
-            </div>
-            <Button className="mt-6 w-full bg-[#ED7D31] font-extrabold text-black">Login</Button>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  </main>
-  )
-}
+            </label>
+          </div>
 
-export default Login
+          <Button className=" bg-[#e94f37] text-black font-bold rounded-ful w-full">
+            Login 
+          </Button>
+          <p className="text-[#393e41]">
+            Don't have an account yet?{' '}
+            <Link href="/register" className="text-[#e94f37]">
+              Register
+            </Link>{' '}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
